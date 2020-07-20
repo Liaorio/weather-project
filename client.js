04d4718c9fc8fc3ab09400caf57479c9480c6091
liao <liaorio@gmail.com>
2020-07-20 19:23:43 -0400

    tweaks for Heroku

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ const message = document.querySelector('#message');
 searchBtn.addEventListener("click", () => {
   message.textContent = 'Loading...';
   const address = searchForm.value;
-  fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
+  fetch(`/weather?address=${address}`).then((res) => {
     res.json().then((data) => {
       const { temp, time } = data;
       const resultStr = `The weather of ${address} at ${time} is ${temp}`;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const utils = require('./utils');
 
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
 
